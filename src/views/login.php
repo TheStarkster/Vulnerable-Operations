b1a0b4494debf6df931663380b5dd3f0eb6c45f0
Gurkaran <sgurkaran2000@gmail.com>
2019-07-27 16:18:11 +0530

    inits

---
+++ src/views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include './masters/headers.php' ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./styles/login.css">
+    <title>Login</title>
+</head>
+<body>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <center>
+      <h5 class="card-title">Enter Your Credentials</h5>
+      <input type="text" placeholder="Username" class="form-control">
+      <input type="password" placeholder="*******" class="form-control">
+      <input type="submit" value="Login" class="btn btn-primary">
+    </center>
+  </div>
+</div>
+</body>
+</html>
---
+++ src/views/masters/headers.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="../../public/vendor/css/bootstrap.css">
+<script src="../../public/vendor/js/bootstrap.js"></script>
